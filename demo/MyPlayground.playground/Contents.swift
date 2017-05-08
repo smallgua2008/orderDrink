bec269a5c3f493b860b0c96f9f4abc1e4053cf55
王君傑 <alex.wang@wangjunjiede-MacBook-Pro.local>
2017-05-08 13:11:57 +0800

    my music app first commit

---
+++ demo/MyPlayground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+1-1
+5+5
+2+7
+
+let numberOfDogs = 6
+
+
+var str1 = "adfdfdfdfdfdfdfdfdfdfkvdkfndkfmnksdlajfkls"
+var str2 = "abc"
+var str3 = str1 + "," + str2
+
+
+var sum = 0
+for i in 1...1000{
+    sum+=1
+    
+}
